f401e1c253c270d51c9e8d23bd8b7f3eb736c5a8
André De Carli Dias <68877082+andredias20@users.noreply.github.com>
2022-05-18 15:55:17 -0300

    adding some code

--- 1 - Learning the basics/FirstClass/Program.cs
+++ 1 - Learning the basics/FirstClass/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace FirstModule {
+
+    class Program{
+
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+    
+
+}
