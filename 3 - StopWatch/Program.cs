011abeece09ceb0a0bbdfe387fc04a197ae9027f
Andre Dias <andre.c.dias2000@gmail.com>
2022-07-12 00:26:56 -0300

    StopWatch pt. 1

--- 3 - StopWatch/Program.cs
+++ 3 - StopWatch/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start(6);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000); 
+            }
+        }
+    }
+}
